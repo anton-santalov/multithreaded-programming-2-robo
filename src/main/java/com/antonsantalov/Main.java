12411aa683211c77a84b8b40038c146d3bf345e9
Anton Santalov <anton.santalov1995@gmail.com>
2024-10-09 04:17:15 +0400

    Functionality implemented.

--- src/main/java/com/antonsantalov/Main.java
+++ src/main/java/com/antonsantalov/Main.java
@@ -1,17 +1,49 @@
 package com.antonsantalov;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    private static final int threadNumber = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+
+        long startTs = System.currentTimeMillis(); // start time
+
+        for (int i = 0; i < threadNumber; i++) {
+            Thread thread = new Thread(Main::addRCountToMap);
+            threads.add(thread);
+            thread.start();
         }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+
+        sizeToFreq.entrySet().stream().sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+            .forEach(it -> System.out.printf("Значение %d повторилось %d раз\n", it.getKey(), it.getValue()));
+    }
+
+    private static void addRCountToMap() {
+        synchronized (sizeToFreq) {
+            String route = generateRoute("RLRFR", 100);
+            long rNumber = route.chars().filter(c -> c == 'R').count();
+            sizeToFreq.put((int) rNumber,
+                sizeToFreq.get((int) rNumber) != null ? sizeToFreq.get((int) rNumber) + 1 : 1);
+        }
+    }
+
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
     }
 }
